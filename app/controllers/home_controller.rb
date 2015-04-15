9aea0843f590f7a54c06c63f8b04ba9a97091162
nalnaji <nalnaji@berkeley.edu>
2015-04-14 20:33:30 -0700

    made partials for chat

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,14 @@ class HomeController < ApplicationController
   def index
     @courses = Course.all
     @locations = []
+    @chat = nil
+
   end
   def get_buildings_for_course
     @course = Course.find(params[:course_id])
     @locations = Location.all
     render 'get_buildings.js'
+  end
+  def get_chatroom
   end
 end
